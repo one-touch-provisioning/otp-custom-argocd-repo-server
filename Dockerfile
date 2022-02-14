fa037eccf27097ecfd1491c13e7b7d35c51e30e9
Ben Swinney <ben.swinney@gmail.com>
2022-02-15 10:46:22 +1100

    Updated to v1.4.2-2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM registry.redhat.io/openshift-gitops-1/argocd-rhel8:v1.4.2
-ENV KUSTOMIZE_PLUGIN_HOME=/etc/kustomize/plugin     POLICY_GENERATOR_VERSION=v1.4.1
+
+ENV KUSTOMIZE_PLUGIN_HOME=/etc/kustomize/plugin
+
 USER 0
-RUN mkdir -p /policy.open-cluster-management.io/v1/policygenerator &&     curl -L       -o /policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator       https://github.com/open-cluster-management/policy-generator-plugin/releases/download//linux-amd64-PolicyGenerator &&     chmod +x /policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator
+RUN mkdir -p /etc/kustomize/plugin/policy.open-cluster-management.io/v1/policygenerator &&     curl -L       -o /etc/kustomize/plugin/policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator       https://github.com/open-cluster-management/policy-generator-plugin/releases/download/v1.4.1/linux-amd64-PolicyGenerator &&     chmod +x /etc/kustomize/plugin/policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator
 USER argocd
--- build.sh
+++ build.sh
@@ -4,26 +4,28 @@
 #IMAGE=`oc get -n openshift-gitops deployment openshift-gitops-cntk-repo-server -o=jsonpath='{@.spec.template.spec.containers[0].image}'`
 IMAGE="registry.redhat.io/openshift-gitops-1/argocd-rhel8"
 IMAGE_VER="v1.4.2"
+POLICY_GENERATOR_VERSION="v1.4.1"
 
 # Build Dockerfile
 echo "Building Dockerfile"
 
 cat <<EOF > Dockerfile
 FROM $IMAGE:$IMAGE_VER
-ENV KUSTOMIZE_PLUGIN_HOME=/etc/kustomize/plugin \
-    POLICY_GENERATOR_VERSION=v1.4.1
+
+ENV KUSTOMIZE_PLUGIN_HOME=/etc/kustomize/plugin
+
 USER 0
-RUN mkdir -p $KUSTOMIZE_PLUGIN_HOME/policy.open-cluster-management.io/v1/policygenerator && \
+RUN mkdir -p /etc/kustomize/plugin/policy.open-cluster-management.io/v1/policygenerator && \
     curl -L \
-      -o $KUSTOMIZE_PLUGIN_HOME/policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator \
+      -o /etc/kustomize/plugin/policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator \
       https://github.com/open-cluster-management/policy-generator-plugin/releases/download/$POLICY_GENERATOR_VERSION/linux-amd64-PolicyGenerator && \
-    chmod +x $KUSTOMIZE_PLUGIN_HOME/policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator
+    chmod +x /etc/kustomize/plugin/policy.open-cluster-management.io/v1/policygenerator/PolicyGenerator
 USER argocd
 EOF
 
 # Build Image
 echo "Building Image"
-IMAGE_TAG="v1.4.2" # Tracks OpenShift GitOps version
+IMAGE_TAG="v1.4.2-2" # Tracks OpenShift GitOps version
 podman build -f Dockerfile --no-cache -t quay.io/benswinneyau/openshift-gitops-repo-server:$IMAGE_TAG .
 
 # Push Image
